62c3622ebff4566144d0c6be733b5ffb19225c0e
Chuzilla <fernando.ferreira.oceguera@gmail.com>
2026-05-10 21:21:04 -0600

    added json for vs code

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
+let countloc=0
+let countawa=0
+let localcount =document.getElementById("localcount")
+let awaycount= document.getElementById("awaycount")
 
 function increment() {
     count += 1
@@ -12,4 +16,15 @@ function save() {
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
+}
+function incrementLocal()
+{
+    countloc+=1
+    localcount.textContent=countloc
+
+}
+function incrementAway()
+{
+    countawa+=1
+    awaycount.textContent=countawa
 }
